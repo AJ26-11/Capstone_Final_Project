634bd2f1063214576dddd4b6d8523778cb089bc3
lamba11 <lambachetna200011@gmail.com>
2023-05-01 21:07:40 +0530

    Third Commit made by Chetna

--- Capstone_Project/api/index.js
+++ Capstone_Project/api/index.js
@@ -21,7 +21,7 @@ const connect = async () => {
   mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!");
   });
-  
+
 
 app.get("/", (req,res)=>{
     res.send("hello First request!")
@@ -36,6 +36,17 @@ app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
+
+app.use((err, req, res, next)=>{
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 
 app.listen(8800, () => {
