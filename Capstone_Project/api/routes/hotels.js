b377dd141b6d02bec0d573a9c6ba63799cb73449
lamba11 <lambachetna200011@gmail.com>
2023-05-01 21:43:32 +0530

    Fifth Commit by Chetna

--- Capstone_Project/api/routes/hotels.js
+++ Capstone_Project/api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
 import { createError } from "../utils/error.js";
-import { deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { deleteHotel, getHotel, getHotels, updateHotel, createHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
